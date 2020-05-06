3776a54c5178fd46337222c7b8b8c49e32a72c3a
Sergio Daniel Medina <medinasergiodaniel@gmail.com>
2020-05-06 15:24:13 -0300

    se agrego mensaje de bienvenida y se conecto el resultado con su funcion

--- prueba/main.py
+++ prueba/main.py
@@ -10,8 +10,9 @@ def calculoDeResultado(primero, segundo, operacion):
 def muestraDeResultado():
  return 1
 
+print "comienzo del programa"
 primerValor = tomaDeDatos()
 operacion = tomaDeOperacion()
 segundoValor = tomaDeDatos()
 resultado = calculoDeResultado(primerValor, segundoValor, operacion)
-muestraDeResultado()
+muestraDeResultado(resultado)
