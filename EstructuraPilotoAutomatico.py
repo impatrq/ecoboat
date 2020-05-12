def69771231f5706f20c9d8a723034c158915615
EstanislaoL <64919740+EstanislaoL@users.noreply.github.com>
2020-05-12 17:20:40 -0300

    Update EstructuraPilotoAutomatico.py

--- EstructuraPilotoAutomatico.py
+++ EstructuraPilotoAutomatico.py
@@ -1,19 +1,40 @@
-def tomarWaypoint():
-	return Waypoint
+import time as tm 
+import numpy as np 
+#uso numpy ya que tiene un mejor manejo de matrices
 
 def DireccionActual():
+	#determinar la direccion actual, esta funcion ya esta en la libreria del gps
+	#para que el gps determine la direccion debemos recorrer unos metros
 	return 1
 
-def DireccionDeseada(direcactual, waypoint):
+def DireccionDeseada(waypoint):
+	#determinar la direccion deseada, esta funcion ya esta en la libreria del gps
+	#al gps le debemos decir hacia donde queremos ir y nos dera la direccion en grados
 	return 1 
 
 def deltaDireccion(direcdeseada, direcactual):
+	#calcular cuantos grados debemos girar
 	return 1
 
 def Girar(deltadirec):
 	return 0
 
+def LlegadaAlWP(destino):
+	#comparar nuestra direccion con el destino
+	return 0
 
-DireccionDeseada(DireccionActual, tomarWaypoint)
-deltadireccion= deltaDireccion(DireccionDeseada, DireccionActual)
-Girar(deltadireccion)
+#en esta array se guardan los waypoints a recorrer 
+#cada waypoint se guarda en una fila distinta
+waypoints= np.empty(TotaldeWaypoints, 2)
+
+#se recorren todos los watpoints uno por uno
+for i in range (0, len(waypoints)):
+
+	#waypoints[i] hace referrencia a la fila entera
+	direcDeseada=DireccionDeseada(waypoints[i])
+
+	#la idea es que corrija el rumbo a lo largo del trayecto cada xx tiempo
+	while(LlegadaAlWP(waypoints[i]) != 1):
+		deltadirec= deltaDireccion(direcDeseada, DireccionActual())
+		Girar(deltadirec)
+		tm.sleep(xx)
