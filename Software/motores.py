c319d71aa75e6e692099f0624135032b48b98972
EstanislaoL <64919740+EstanislaoL@users.noreply.github.com>
2020-05-15 19:11:44 -0300

    motores
    libreria para controlar los motores
    falta imrementacion de pwm

---
+++ Software/motores.py
@@ -0,0 +1,82 @@
+import RPi.GPIO as gpio 
+import time 
+import numpy as np 
+
+estados=np.array([
+	["True","False","False","False"],
+	["False","True","False","False"],
+	["False","False","True","False"],
+	["False","False","False","True"],
+	])
+
+class PaP():
+
+	def __init__(self, pin1, pin2, pin3, pin4):
+
+		self.pin1= pin1
+		self.pin2= pin2
+		self.pin3= pin3
+		self.pin4= pin4
+
+		gpio.setup(self.pin1, gpio.OUT)
+		gpio.setup(self.pin2, gpio.OUT)
+		gpio.setup(self.pin3, gpio.OUT)
+		gpio.setup(self.pin4, gpio.OUT)
+
+	def girarH(self, grados):
+		i=grados*512
+		for t in range (0, i):
+			for e in range (0,4):
+				gpio.output(self.pin1, estados[e, 0])
+				gpio.output(self.pin2, estados[e, 1])
+				gpio.output(self.pin3, estados[e, 2])
+				gpio.output(self.pin4, estados[e, 3])
+				time.sleep(0.005)
+
+		return 0
+
+	def girarAH(self, grados):
+		i=grados*512
+		for t in range (0, i):
+			for e in range (0,4):
+				gpio.output(self.pin1, estados[e, 3])
+				gpio.output(self.pin2, estados[e, 2])
+				gpio.output(self.pin3, estados[e, 1])
+				gpio.output(self.pin4, estados[e, 0])
+				time.sleep(0.005)
+			t+=1
+
+		return 0
+
+class puenteH():
+
+	def __init__(self, pin1, pin2):
+
+		self.pin1= pin1
+		self.pin2= pin2
+
+		gpio.setup(self.pin1, gpio.OUT)
+		gpio.setup(self.pin2, gpio.OUT)
+
+	def girarD(self):
+
+		gpio.output(self.pin2, "False")
+		gpio.output(self.pin1, "True")
+		
+		return 0
+
+	def girarI(self):
+
+		gpio.output(self.pin1, "False")
+		gpio.output(self.pin2, "True")
+
+		return 0
+
+	def detener(self):
+
+		gpio.output(self.pin1, "False")
+		gpio.output(self.pin2, "False")
+
+		return 0
+
+
