216488f634894fa5eca077ee041180685d774131
EstanislaoL <64919740+EstanislaoL@users.noreply.github.com>
2020-05-15 23:47:36 -0300

    PilotoAuto
    desarrolle la funcion de giro de direccion

--- Software/EstructuraPilotoAutomatico.py
+++ Software/EstructuraPilotoAutomatico.py
@@ -1,5 +1,6 @@
 import time as tm 
 import numpy as np 
+import motores as mt 
 #uso numpy ya que tiene un mejor manejo de matrices
 
 def DireccionActual():
@@ -12,16 +13,46 @@ def DireccionDeseada(waypoint):
 	#al gps le debemos decir hacia donde queremos ir y nos dera la direccion en grados
 	return 1 
 
-def deltaDireccion(direcdeseada, direcactual):
-	#calcular cuantos grados debemos girar
-	return 1
+def Girar(motor, waypoint):
 
-def Girar(deltadirec):
+	#primero hace un primer chequeo de la diferencia de direccion
+	DD= DireccionDeseada(waypoint)
+	DA= DireccionActual()
+	DeltaD= DD - DA
+
+	#si no es demaciado no gira
+	if (abs(DeltaD)<= 10):
+		return 0
+	#si es mucho lo corrije
+	else:
+		#verifica si hay que girar a la derecha o izquierda
+		if(DeltaD<0):
+			motor.girarD(10)
+			giro=1
+		else:
+			motor.girarI(10)
+			giro=0
+
+		#se mantiene girando hasta que corrija el rumbo
+		while(abs(DeltaD) >= 5):
+			DD= DireccionDeseada(waypoint)
+			DA= DireccionActual()
+			DeltaD= DD - DA
+			tm.sleep(0.5)
+
+		#vuelve a su posicion el timon
+		if (giro==1):
+			motor.girarI(10)
+		else:
+			motor.girarD(10)
+
 	return 0
 
 def LlegadaAlWP(destino):
 	#comparar nuestra direccion con el destino
 	return 0
+
+timon= mt.PaP(1, 2, 3, 4)
 
 #en esta array se guardan los waypoints a recorrer 
 #cada waypoint se guarda en una fila distinta
@@ -29,12 +60,7 @@ waypoints= np.empty(TotaldeWaypoints, 2)
 
 #se recorren todos los watpoints uno por uno
 for i in range (0, len(waypoints)):
-
-	#waypoints[i] hace referrencia a la fila entera
-	direcDeseada=DireccionDeseada(waypoints[i])
-
 	#la idea es que corrija el rumbo a lo largo del trayecto cada xx tiempo
 	while(LlegadaAlWP(waypoints[i]) != 1):
-		deltadirec= deltaDireccion(direcDeseada, DireccionActual())
-		Girar(deltadirec)
-		tm.sleep(xx)
+		Girar(timon, waypoints[i])
+		tm.sleep(30)
---
+++ Software/pruebas.py
@@ -0,0 +1,14 @@
+import motores as mt 
+
+a= mt.puenteH(1,2)
+
+
+def sumar(a, b):
+	print(a+b)
+	return 0
+
+def girar(a, b):
+	sumar(a,b)
+	return 0
+
+girar(1,2)
