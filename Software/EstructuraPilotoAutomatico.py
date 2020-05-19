ae023aa48c6ab9a00b181b7106b7641982d07500
EstanislaoL <64919740+EstanislaoL@users.noreply.github.com>
2020-05-19 11:24:29 -0300

    Update EstructuraPilotoAutomatico.py
    cambie un par de cositas

--- Software/EstructuraPilotoAutomatico.py
+++ Software/EstructuraPilotoAutomatico.py
@@ -1,7 +1,7 @@
 import time as tm 
 import numpy as np 
 import motores as mt 
-#uso numpy ya que tiene un mejor manejo de matrices
+from pynmea2 import nmea
 
 def DireccionActual():
 	#determinar la direccion actual, esta funcion ya esta en la libreria del gps
@@ -42,15 +42,17 @@ def Girar(motor, waypoint):
 
 		#vuelve a su posicion el timon
 		if (giro==1):
-			motor.girarH(10)
-		else:
 			motor.girarAH(10)
+		else:
+			motor.girarH(10)
 
 	return 0
 
 def LlegadaAlWP(destino):
 	#comparar nuestra direccion con el destino
 	return 0
+
+
 
 
 timon= mt.PaP(1, 2, 3, 4)
@@ -60,7 +62,7 @@ motorDirec.girarD()
 
 #en esta array se guardan los waypoints a recorrer 
 #cada waypoint se guarda en una fila distinta
-waypoints= np.empty(TotaldeWaypoints, 2)
+waypoints= np.empty((TotaldeWaypoints, 2))
 
 #se recorren todos los watpoints uno por uno
 for i in range (0, len(waypoints)):
