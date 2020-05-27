eb760fea791dc50c8645b4798718feea4b00a836
EstanislaoL <64919740+EstanislaoL@users.noreply.github.com>
2020-05-27 16:13:27 -0300

    modulos RF
    cambios en master y slave

--- Software/Módulos de RF/Módulo Master.py
+++ Software/Módulos de RF/Módulo Master.py
@@ -8,12 +8,12 @@ GPIO.setmode(GPIO.BCM)
 class transmisor():
 
 	def __init__(self, pin1, pin2):
-                self.pin1=pin1
+        self.pin1=pin1
 	    self.pin2=pin2
 
 	    #configuracion del modulo
 
-                pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+        pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
 
 		radio = NRF24(GPIO, spidev.SpiDev())
 		radio.begin(self.pin1, self.pin2)
@@ -35,15 +35,15 @@ class transmisor():
 		radio.printDetails()
 
 def comandoZarpar():
-        #Envía el comando para que el barco zarpe.
-        return comando
+    #Envía el comando para que el barco zarpe.
+    if zarpar== 1:
+    	return Si 
+    else:
+    	return No
 
 def recivirDatos():
-        radio.startListening()
-        while not radio.available(0):
-                time.sleep(1/100)
-        #Recive todos los datos y los guarda en un array.
-        return datos
+    #Recive todos los datos y los guarda en un array.
+    return datos
 
 while True:
 	comando = comandoZarpar()
@@ -51,16 +51,17 @@ while True:
 	radio.Write(mensaje)
 	print ("Se mandó el comando de zarpar.")
 
-        #Chequea si el slave recivió el comando.
+    #Chequea si el slave recivió el comando.
 	if radio.isAckPayloadAvailable ():
-                returnedPL = []
-                radio.read(returnedPL, radio.getDynamicPayloadSize())
-                print (returnedPL)
-                #Si se recive el ackPL se empiezan a recivir los datos
-                while radio.available(0):
-                        recivirDatos()
-                radio.stopListening()
+        returnedPL = []
+        radio.read(returnedPL, radio.getDynamicPayloadSize())
+        print (returnedPL)
+        #Si se recive el ackPL se empiezan a recivir los datos
+        radio.startListening()
+        while radio.available(0):
+            recivirDatos()
+        radio.stopListening()
                 
 	else:
-                print ("No se recivió el mensaje")
-                time.sleep(1)
+        print ("No se recivió el mensaje")
+        time.sleep(1)
