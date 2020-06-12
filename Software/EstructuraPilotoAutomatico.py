f2d6eaf859a130123f5cc1c88ec188a75743ac0e
EstanislaoL <64919740+EstanislaoL@users.noreply.github.com>
2020-06-12 13:35:57 -0300

    Threading
    uni los progamas del PA, modulos RF y GPS para utilizar Threading a traves de u unico codigo

--- Software/EstructuraPilotoAutomatico.py
+++ Software/EstructuraPilotoAutomatico.py
@@ -1,101 +1,158 @@
-import time as tm 
+import time
+import threading 
+import pynmea2 as nmea
 import numpy as np 
 import motores as mt 
 import math
 
-def distancia(lat1, lng1 , waypoint):
-	lat2=waypoint[0, 0]
-	lng2=waypoint[0, 1]
-	r=6371000
-	c=(math.pi)/180
-	#Fórmula de haversine
-	d = 2*r*asin(sqrt(sin(c*(lat2-lat1)/2)**2 + cos(c*lat1)*cos(c*lat2)*sin(c*(long2-long1)/2)**2))
-	return d
+#//////////////// defino al objeto gps el cual va a guardar las variables/////////////
 
-def cursoHacia(lat1, lng1, waypoint):
-	lat2=waypoint[0, 0]
-	lng2=waypoint[0, 1]
-	dlon=lng2-lng1 
-	a1=sin(dlon)*cos(lat2)
-	a2=(cos(lat1)*sin(lat2))-(sin(lat1)*cos(lat2)*cos(dlon))
-	curso=atan2(a1, a2)
+class gps():
 
-	if(curso < 0):
-		curso += 2*math.pi
+	def __init__(sel):
+		self.lat=0
+		self.lng=0
+		self.cur=0
+	return 0
 
-	return degrees(curso)
+	def disponible(self):
+		#while que espere a que haya informacion en el gps
+	return 1
 
-def EvitarObstaculos():
+#//////////////// funciones de Piloto Automatico////////////////
+
+def pilotoAutomatico():
+
+	def EvitarObstaculos():
     #Sensores para evitar que el barco choque
     return 1
 
-def DireccionActual():
-	Gps.lectura()
-	return Gps.cur
+	def distancia(lat1, lng1 , waypoint):
+		lat2=waypoint[0, 0]
+		lng2=waypoint[0, 1]
+		r=6371000
+		c=(math.pi)/180
+		#Fórmula de haversine
+		d = 2*r*asin(sqrt(sin(c*(lat2-lat1)/2)**2 + cos(c*lat1)*cos(c*lat2)*sin(c*(long2-long1)/2)**2))
+		return d
 
-def DireccionDeseada(waypoint):
-	Gps.lectura()
-	return cursoHacia(Gps.lat, Gps.lng, waypoint)
+	def DireccionDeseada(lat1, lng1, waypoint):
+		lat2=waypoint[0, 0]
+		lng2=waypoint[0, 1]
+		dlon=lng2-lng1 
+		a1=sin(dlon)*cos(lat2)
+		a2=(cos(lat1)*sin(lat2))-(sin(lat1)*cos(lat2)*cos(dlon))
+		curso=atan2(a1, a2)
 
-def Girar(waypoint):
+		if(curso < 0):
+			curso += 2*math.pi
 
-	#primero hace un primer chequeo de la diferencia de direccion
-	DD= DireccionDeseada(waypoint)
-	DA= DireccionActual()
-	DeltaD= DD - DA
+		return degrees(curso)
 
-	#si no es demaciado no gira
-	if (abs(DeltaD)<= 10):
-		return 0
-	#si es mucho lo corrije
-	else:
-		#verifica si hay que girar a la derecha o izquierda
-		if(DeltaD<0):
-			timon.girarH(10)
-			giro=1
+	def Girar(waypoint):
+
+		#primero hace un primer chequeo de la diferencia de direccion
+		DD= DireccionDeseada(Gps.lat, Gps.lng, waypoint)
+		DA= Gps.cur #Direccion actual dado por el modulo gps
+		DeltaD= DD - DA
+
+		#si no es demaciado no gira
+		if (abs(DeltaD)<= 10):
+			return 0
+		#si es mucho lo corrije
 		else:
-			timon.girarAH(10)
-			giro=0
+			#verifica si hay que girar a la derecha o izquierda
+			if(DeltaD<0):
+				timon.girarH(10)
+				giro=1
+			else:
+				timon.girarAH(10)
+				giro=0
 
-		#se mantiene girando hasta que corrija el rumbo
-		while(abs(DeltaD) >= 5):
-			DD= DireccionDeseada(waypoint)
-			DA= DireccionActual()
-			DeltaD= DD - DA
-			tm.sleep(0.5)
+			#se mantiene girando hasta que corrija el rumbo
+			while(abs(DeltaD) >= 5):
+				DD= DireccionDeseada(Gps.lat, Gps.lng, waypoint)
+				DA= Gps.cur
+				DeltaD= DD - DA
+				tm.sleep(0.5)
 
-		#vuelve a su posicion el timon
-		if (giro==1):
-			timon.girarAH(10)
-		else:
-			timon.girarH(10)
+			#vuelve a su posicion el timon
+			if (giro==1):
+				timon.girarAH(10)
+			else:
+				timon.girarH(10)
 
-	return 0
-
-def LlegadaAlWP(destino):
-	#comparar nuestra direccion con el destino
-	Gps.lectura()
-	dis= distancia(Gps.lat, Gps.lng, destino)
-
-	if(abs(dis) <= 5):
 		return 0
-	else:
-		return 1
-	
-#inicio el gps
 
-timon= mt.PaP(1, 2, 3, 4)
-motorDirec= mt.puenteH(1,2)
-#meto primera
-motorDirec.girarD()
+	def LlegadaAlWP(destino):
+		#comparar nuestra direccion con el destino
+		dis= distancia(Gps.lat, Gps.lng, destino)
 
-#en esta array se guardan los waypoints a recorrer 
-#cada waypoint se guarda en una fila distinta
-waypoints= np.empty((TotaldeWaypoints, 2))
+		if(abs(dis) <= 5):
+			return 0
+		else:
+			return 1
 
-#se recorren todos los watpoints uno por uno
-for i in range (0, len(waypoints)):
-	#la idea es que corrija el rumbo a lo largo del trayecto cada xx tiempo
-	while(LlegadaAlWP(waypoints[i]) != 1):
-		Girar(waypoints[i])
-		tm.sleep(0.05)
+	timon= mt.PaP(1, 2, 3, 4)
+	motorDirec= mt.puenteH(1,2)
+	#meto primera
+	motorDirec.girarD()
+
+	#en esta array se guardan los waypoints a recorrer 
+	#cada waypoint se guarda en una fila distinta
+	waypoints= np.empty((TotaldeWaypoints, 2))
+
+	#se recorren todos los watpoints uno por uno
+	for i in range (0, len(waypoints)):
+		#la idea es que corrija el rumbo a lo largo del trayecto cada xx tiempo
+		while(LlegadaAlWP(waypoints[i]) != 1):
+			Girar(waypoints[i])
+			time.sleep(5)
+
+#//////////////// funciones de RF //////////////////////
+
+def comRF():
+	#codigo de modulos RF
+
+#//////////////// funciones de GPS //////////////////////
+
+def GPSlectura():
+
+	def lectura(self):
+		port="/dev/ttyAMA0"
+		ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+		dataout=pynmea2.NMEAStreamReader()
+		data=ser.readline()
+
+		if data[0:6] == "$GPRMC":
+			datos=pynmea2.parse(newdata)
+			Gps.lat=datos.latitude
+			Gps.lng=datos.longitude
+			Gps.cur=datos.direction
+
+		time.sleep(0.05)
+		return 0
+
+#//////////////// inicio el gps //////////////////////////
+
+Gps= gps()
+
+#espero a que haya data para arrancar
+
+while True:
+	if (Gps.disponible==1):
+		break
+
+#////////////// inicio los hilos ////////////////////
+
+TPA = threading.Thread(name="PA", target=pilotoAutomatico)
+TRF = threading.Thread(name="RF", target=comRF)
+TGPS = threading.Thread(name="GPS", target=GPSlectura)
+
+TPA.start()
+TRF.start()
+TGPS.start()
+
+TPA.join()
+TRF.join()
+TGPS.join()
--- Software/pruebas.py
+++ Software/pruebas.py
@@ -1,6 +1,25 @@
-import numpy as np 
+import threading
+import time
 
-a= np.full((2,3),4)
+variable1=0
 
-print(a[1])
-print(a[:, 2])
+def prueba1(dato):
+	time.sleep(1)
+	variable1=dato
+	return 0
+
+def prueba2():
+	print(variable1)
+	time.sleep(1.5)
+	print(variable1)
+	return 0
+
+t1 = threading.Thread(name="hilo1", target=prueba1, args=(10, ))
+t2 = threading.Thread(name="hilo2", target=prueba2)
+
+t1.start()
+t2.start()
+
+
+t1.join()
+t2.join()
