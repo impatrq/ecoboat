be80ceadb9ed718d257738000634bf587c0acaca
Facu Di Toro <56963307+difacundo@users.noreply.github.com>
2020-06-23 13:05:07 -0300

    Conexion Remota
    Use remote para conectarme desde dos redes diferentes.

--- Software/Sockets/PruebaCliente.py
+++ Software/Sockets/PruebaCliente.py
@@ -1,8 +1,8 @@
 import socket
 
 #Defino el puerto y la dirección IP del servidor.
-PUERTO = 7000
-SERVIDOR = "192.168.56.1"
+PUERTO = 29999
+SERVIDOR = "127.0.0.1"
 ADDR = (SERVIDOR, PUERTO)
 FORMATO = 'utf-8' #Formato de bytes que se va a usar para transmitir
 HEADER = 64 #Cantidad de bytes que se va a usar para mandar la longitud del dato
